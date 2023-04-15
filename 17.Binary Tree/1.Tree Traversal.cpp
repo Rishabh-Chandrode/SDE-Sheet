d836895785cc13954fef9402f305390a0d859779
Rishabh <rishabhchandrode@gmail.com>
2023-04-15 15:55:37 +0530

    Tree Traversal

---
+++ 17.Binary Tree/1.Tree Traversal.cpp
@@ -0,0 +1,94 @@
+// Inorder Traversal
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        while(root!=NULL){
+            if(root->left){
+                TreeNode *prev = root->left;
+                while(prev->right && prev->right != root) prev = prev->right;
+                if(prev->right==NULL){
+                    prev->right = root;
+                    root = root->left;
+                }else{
+                    prev->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }else{
+                ans.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
+
+
+// Preorder Traversal
+
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+
+        while(root!=NULL){
+            
+            if(root->left){
+                TreeNode *prev = root->left;
+                
+                while(prev->right && prev->right!=root){
+                    
+                    prev = prev->right;
+                }
+                if(prev->right == NULL){
+                    ans.push_back(root->val);
+                    prev->right = root;
+                    root = root->left;
+                }else{
+                    prev->right = NULL;
+                    root = root->right;
+                }
+            }else{
+                ans.push_back(root->val);
+                root = root->right;
+            }
+        }
+        return ans;
+    }
+};
+
+
+Post order Traversal
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if(!root) return ans;
+        while(root!=NULL){
+            if(root->right){
+                TreeNode *prev = root->right;
+                while(prev->left && prev->left!=root){
+                    prev = prev->left;
+                }
+                if(prev->left == NULL){
+                    ans.insert(ans.begin(),root->val);
+                    prev->left = root;
+                    root = root->right;
+                }else{
+                    prev->left = NULL;
+                    root = root->left;
+                }
+            }else{
+                ans.insert(ans.begin(),root->val);
+                root =root->left;
+            }
+        }
+        return ans;
+    }
+}; 
---
+++ 17.Binary Tree/readme.md
@@ -0,0 +1,2 @@
+## 1,2,3,4,5)Tree Traversal 
+                Tree traversal using Morris Traversal
